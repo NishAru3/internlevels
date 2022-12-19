bb907c217b88abb10096460811949b534204ed41
Nishanth Arumugam <nishantharu3@gmail.com>
2022-12-19 00:49:33 -0800

    Updated Images

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # internlevels
 
+
+[image]:salaryvisualizer.png
+[link]:salaryvisualizer.html
+
+[![Salary Visualizer][image]][link]
+[*Go to Visualizer*][link]
+
+
 Python script that takes Pitt CS Internship Links and prints out the median salary as stated by Levels.fyi, if available.
+- Parses Pitt CS with HTTP requests
+- Compares information to Levels.fyi website calls
+- Presents sorted data in files
+- Creates visualization to find and understand company behavior
 
 
-[The List in the order of PittCS](pittCSList.md)
+Here are the lists
 
-[The List ordered by highest salary](orderedList.md)
+
+[- in the order of PittCS](pittCSList.md)
+
+[- ordered by highest salary](orderedList.md)
 
 All credits go to Pitt CS Club for creating and maintaining the [list of Summer '23 internships](https://github.com/pittcsc/Summer2023-Internships)!
 
-<embed type="text/html" src="test.html" width="600" height="400"></embed>
--- salaryvisualizer.py
+++ salaryvisualizer.py
@@ -1,4 +1,6 @@
+from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure, show, output_file
+from bokeh.io import export_png
 names = []
 x = []
 y = []
@@ -13,13 +15,21 @@ for line in data:
 
 data.close()
 
+TOOLTIPS = [
+    ("Company", "@name"),
+    ("Intern Hourly", "@y"),
+    ("Median SWE Salary", "@x")
+]
 
-p = figure(title="Intern Hourly vs Median SWE Salary", x_axis_label='Median SWE Salary ($1000)', y_axis_label='Intern Hourly ($1)')
+source = ColumnDataSource(data=dict(x=x,y=y,name=names))
+
+p = figure(title="Intern Hourly vs Median SWE Salary", x_axis_label='Median SWE Salary ($1000)', y_axis_label='Intern Hourly ($/hr)', tooltips=TOOLTIPS)
 # p.x_range = range(0,int(max(x))+10**len(str(int(max(x)))))
 # p.y_range = range(0,int(max[y])+10**len(str(int(max(y)))))
 p.width = 1400
 p.height = 800
-p.circle(x,y, size=10, color="navy", alpha=0.5)
+p.circle(source=source, size=15, color="navy", alpha=0.5)
 # p.line(x, y, legend_label="Temp.", line_width=2)
-output_file("test.html")
-show(p)
+output_file("salaryvisualizer.html")
+export_png(p, filename="salaryvisualizer.png")
+# show(p)
